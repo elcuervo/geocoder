d9754522cd9271043024b893deb4e3a1ffa2fce0
Bruno Aguirre <yo@brunoaguirre.com>
2013-07-09 15:35:17 -0300

    Refactored with new utilities

--- geocoder.go
+++ geocoder.go
@@ -1,55 +1,96 @@
 package geocoder
 
 import (
-        "fmt"
-        "encoding/json"
-        "net/http"
-        "net/url"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
 )
 
 type Coordinates struct {
-        Lat, Lng float64
+	Lat, Lng float64
+}
+
+type City struct {
+	Name        string
+	Coordinates Coordinates
 }
 
 type Response struct {
-        Status  string
-        Results []*Result `json:"address_components"`
+	Status  string
+	Results []*Result
 }
 
 type Result struct {
-        Address  string   `json:"formatted_address"`
-        Geometry Geometry
+	Addresses []*Address `json:"address_components"`
+	Geometry  Geometry
 }
 
-func (r *Result) Coordinates() Coordinates {
-        return r.Geometry.Location
+type Address struct {
+	Name  string `json:"long_name"`
+	Types []string
 }
 
 type Geometry struct {
-        Location Coordinates
+	Location Coordinates
 }
 
 const api = "http://maps.googleapis.com/maps/api/geocode/json"
 
 type Geocoder struct{}
 
-func (g *Geocoder) City(address string) (*Result, error) {
-        safe_address := url.QueryEscape(address)
-        uri := fmt.Sprintf("%s?sensor=false&address=%s", api, safe_address)
-        data, err := http.Get(uri)
+func (g *Geocoder) City(address string) (*City, error) {
+	safe_address := url.QueryEscape(address)
+	uri := fmt.Sprintf("%s?sensor=false&address=%s", api, safe_address)
+	return g.fetch(uri)
+}
 
-        if err != nil {
-                panic(err)
-        }
+func (g *Geocoder) Coords(lat float64, lng float64) (*City, error) {
+	uri := fmt.Sprintf("%s?sensor=false&latlng=%f,%f", api, lat, lng)
+	return g.fetch(uri)
+}
 
-        defer data.Body.Close()
+func (g *Geocoder) possibleCityName(results []*Result) string {
+	types := []string{
+		"locality",
+		"sublocality",
+		"administrative_area_level_3",
+		"administrative_area_level_2"}
 
-        response := new(Response)
-        err = json.NewDecoder(data.Body).Decode(response)
+	for _, possible := range types {
+		for _, result := range results {
+			for _, address := range result.Addresses {
+				for _, city_type := range address.Types {
+					if possible == city_type {
+					        return address.Name
+					}
+				}
+			}
+		}
+	}
 
-        if err != nil {
-                panic(err)
-        }
+	return ""
+}
 
-        return response.Results[0], nil
+func (g *Geocoder) fetch(uri string) (*City, error) {
+	data, err := http.Get(uri)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer data.Body.Close()
+
+	res := new(Response)
+	err = json.NewDecoder(data.Body).Decode(res)
+
+	if err != nil {
+		panic(err)
+	}
+
+	city := &City{
+		g.possibleCityName(res.Results),
+		res.Results[0].Geometry.Location}
+
+	return city, nil
 }
--- geocoder_test.go
+++ geocoder_test.go
@@ -3,10 +3,30 @@ package geocoder
 import "testing"
 
 func TestCity(t *testing.T) {
-        g := new(Geocoder)
-        _, err := g.City("Montevideo")
+	g := new(Geocoder)
+	city, err := g.City("Montevideo")
 
-        if err != nil {
-                t.Errorf("Not able to geocode")
-        }
+	if err != nil {
+		t.Errorf("Not able to geocode")
+	}
+
+	expected := Coordinates{-34.8836111, -56.1819444}
+
+	if city.Coordinates != expected {
+		t.Error("Unexpected coordinates for Montevideo")
+	}
+
+}
+
+func TestCoords(t *testing.T) {
+	g := new(Geocoder)
+	city, err := g.Coords(-34.8836111, -56.1819444)
+
+	if err != nil {
+		t.Errorf("Not able to geocode")
+	}
+
+	if city.Name != "Montevideo" {
+		t.Errorf("Unexpected city name:(%s) for Montevideo coordinates", city.Name)
+	}
 }
